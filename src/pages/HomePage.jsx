7ff934eb438178c718199024e470b8f757542a4f
Kevin <trupiakevin@gmail.com>
2026-02-25 18:07:00 +0100

    feat : effettuata la chiamata ajax

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,57 @@
 
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import MovieCard from "../components/MovieCard";
 
-function HomePage() {
+function Homepage() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+        axios.get("http://localhost:3000/api/movies")
+
+            .then((response) => {
+
+                setMovies(response.data);
+
+            })
+
+            .catch((error) => {
+
+                console.log(error);
+
+            });
+
+    }, []);
+
+
     return (
+
         <div>
 
             <h1>Lista Film</h1>
 
             <div className="row">
 
-                <MovieCard
-                    id={1}
-                    title="Inception"
-                    image="/images/inception.jpg"
-                />
+                {movies.map(movie => (
 
-                <MovieCard
-                    id={2}
-                    title="Interstellar"
-                    image="/images/interstellar.jpg"
-                />
+                    <MovieCard
+                        key={movie.id}
+                        id={movie.id}
+                        title={movie.title}
+                        image={"/images/" + movie.image}
+                    />
 
-                <MovieCard
-                    id={3}
-                    title="Matrix"
-                    image="/images/matrix.jpg"
-                />
-
-                <MovieCard
-                    id={4}
-                    title="The Godfather"
-                    image="/images/the_godfather.jpg"
-                />
-
-                <MovieCard
-                    id={5}
-                    title="Titanic"
-                    image="/images/titanic.jpg"
-                />
+                ))}
 
             </div>
 
         </div>
+
     );
+
 }
 
-export default HomePage;
+export default Homepage;
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,21 +1,111 @@
 
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function MovieDetail() {
 
     const { id } = useParams();
 
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+
+        axios.get("http://localhost:3000/api/movies/" + id)
+
+            .then((response) => {
+
+                setMovie(response.data);
+
+            })
+
+            .catch((error) => {
+
+                console.log(error);
+
+            });
+
+    }, [id]);
+
+
+
+    if (!movie) {
+
+        return <h2>Caricamento...</h2>;
+
+    }
+
+
     return (
 
         <div>
 
-            <h1>Dettaglio Film</h1>
+            <h1>{movie.title}</h1>
 
-            <p>ID film: {id}</p>
+            <img
+                src={"/images/" + movie.image}
+                width="300"
+            />
+
+            <p>
+
+                <strong>Regista:</strong> {movie.director}
+
+            </p>
+
+            <p>
+
+                <strong>Genere:</strong> {movie.genre}
+
+            </p>
+
+            <p>
+
+                <strong>Anno:</strong> {movie.release_year}
+
+            </p>
+
+            <p>
+
+                {movie.abstract}
+
+            </p>
+
+            <h3>Recensioni</h3>
+
+            {movie.reviews.map(review => (
+
+                <div key={review.id}>
+
+                    <p>
+
+                        <strong>{review.name}</strong>
+
+                    </p>
+
+                    <p>
+
+                        Voto: {review.vote}
+
+                    </p>
+
+                    <p>
+
+                        {review.text}
+
+                    </p>
+
+                    <hr />
+
+                </div>
+
+            ))}
 
         </div>
 
     );
+
 }
 
 export default MovieDetail;
